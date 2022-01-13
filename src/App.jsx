fda96d375b2b2496e8114ee1b7dcab1cc1fa119f
NGR <ngr112001@gmail.com>
2022-01-13 12:39:05 +0545

    pwa react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div>
+      <h1>Namesta!</h1>
+      <p>pwa app</p>
+    </div>
+  );
+}
+
+export default App;
